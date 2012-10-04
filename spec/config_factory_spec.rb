f4dfef3f2a24569b1841b47b6506fd2848fdd1b4
liyijie <liyijie825@gmail.com>
2012-10-04 11:51:59 +0800

    adjust spec

--- spec/config_factory_spec.rb
+++
@@ -1,9 +0,0 @@
-# encoding : utf-8
-require "spec_helper"
-
-describe ConfigConvert do
-	it "should not be nil" do
-		rule_config = ConfigConvert.load_excel("../config/重点数据核查规则0806.xls")
-		rule_config.should_not be_nil
-	end
-end
